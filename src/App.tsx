869cdc2e50277d2947aed2f748920f9a90343222
Stephen Koo <stephenyrkoo@gmail.com>
2022-09-20 10:53:29 +1000

    feat(wishlist): add grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BookInfoPanel } from "ui/BookInfoPanel";
-import "./App.css";
+import styles from "./App.module.css";
 import { WishList } from "./ui/WishList";
 import books from "data/books.json";
 import { useState } from "react";
@@ -9,7 +9,7 @@ const App = () => {
   const [bookToDisplay, setBookToDisplay] = useState<Book | null>(null);
 
   return (
-    <main>
+    <main className={styles.app}>
       <WishList books={books} setBookToDisplay={setBookToDisplay} />
       {bookToDisplay && <BookInfoPanel book={bookToDisplay} />}
     </main>
