6ac61fae36e81d30b12099f6f394a492d3f065a9
Stephen Koo <stephenyrkoo@gmail.com>
2022-09-20 13:19:42 +1000

    feat(wishlist): add book info panel grid

--- src/ui/BookInfoPanel/index.tsx
+++ src/ui/BookInfoPanel/index.tsx
@@ -7,11 +7,16 @@ type BookInfoPanelProps = {
 
 export const BookInfoPanel = ({ book }: BookInfoPanelProps) => (
   <article className={styles.bookInfoPanel}>
-    <h3>{book.title}</h3>
-    <div>{book.author}</div>
-    <b>Publisher&apos;s summary</b>
-    <p>{book.publisherSummary}</p>
+    <div className={styles.titleAndAuthorContainer}>
+      <h3>{book.title}</h3>
+      <div>{book.author}</div>
+    </div>
+    <div className={styles.publishersSummaryContainer}>
+      <h5>Publisher&apos;s summary</h5>
+      <p>{book.publisherSummary}</p>
+    </div>
     <img
+      className={styles.bookCover}
       src={book.imageUrl}
       alt={`${book.title} book cover`}
       role="presentation"
